66344fb38d0c12a8f72da44cecb24b102ecf2f24
Gabor Toth <44711404+Toth-Gabor@users.noreply.github.com>
2019-07-24 11:09:37 +0200

    Add project files.

---
+++ Process-Note/ProcessForm.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace Process_Note
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.metroTextBox1 = new MetroFramework.Controls.MetroTextBox();
+            this.metroButton2 = new MetroFramework.Controls.MetroButton();
+            this.listView = new System.Windows.Forms.ListView();
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.start = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cpu = new System.Windows.Forms.TextBox();
+            this.running = new System.Windows.Forms.TextBox();
+            this.memory = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.comment = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // metroTextBox1
+            // 
+            // 
+            // 
+            // 
+            this.metroTextBox1.CustomButton.Image = null;
+            this.metroTextBox1.CustomButton.Location = new System.Drawing.Point(-20, 2);
+            this.metroTextBox1.CustomButton.Name = "";
+            this.metroTextBox1.CustomButton.Size = new System.Drawing.Size(17, 17);
+            this.metroTextBox1.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.metroTextBox1.CustomButton.TabIndex = 1;
+            this.metroTextBox1.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.metroTextBox1.CustomButton.UseSelectable = true;
+            this.metroTextBox1.CustomButton.Visible = false;
+            this.metroTextBox1.Lines = new string[0];
+            this.metroTextBox1.Location = new System.Drawing.Point(0, 0);
+            this.metroTextBox1.MaxLength = 32767;
+            this.metroTextBox1.Name = "metroTextBox1";
+            this.metroTextBox1.PasswordChar = '\0';
+            this.metroTextBox1.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.metroTextBox1.SelectedText = "";
+            this.metroTextBox1.SelectionLength = 0;
+            this.metroTextBox1.SelectionStart = 0;
+            this.metroTextBox1.ShortcutsEnabled = true;
+            this.metroTextBox1.Size = new System.Drawing.Size(0, 22);
+            this.metroTextBox1.TabIndex = 15;
+            this.metroTextBox1.UseSelectable = true;
+            this.metroTextBox1.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.metroTextBox1.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // metroButton2
+            // 
+            this.metroButton2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.metroButton2.Location = new System.Drawing.Point(328, 281);
+            this.metroButton2.Name = "metroButton2";
+            this.metroButton2.Size = new System.Drawing.Size(75, 23);
+            this.metroButton2.TabIndex = 10;
+            this.metroButton2.Text = "Refresh";
+            this.metroButton2.UseSelectable = true;
+            this.metroButton2.Click += new System.EventHandler(this.MetroButton2_Click);
+            // 
+            // listView
+            // 
+            this.listView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader5,
+            this.columnHeader1});
+            this.listView.FullRowSelect = true;
+            this.listView.HideSelection = false;
+            this.listView.Location = new System.Drawing.Point(23, 63);
+            this.listView.Name = "listView";
+            this.listView.Size = new System.Drawing.Size(240, 298);
+            this.listView.TabIndex = 14;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            this.listView.View = System.Windows.Forms.View.Details;
+            this.listView.SelectedIndexChanged += new System.EventHandler(this.ListView_SelectedIndexChanged);
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "PID";
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Process Name";
+            this.columnHeader1.Width = 150;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(603, 99);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(40, 20);
+            this.textBox1.TabIndex = 17;
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(328, 99);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(128, 20);
+            this.start.TabIndex = 18;
+            this.start.TextChanged += new System.EventHandler(this.TextBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(600, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "Threds Count";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(325, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "Start Time";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(505, 75);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 13);
+            this.label3.TabIndex = 21;
+            this.label3.Text = "CPU Usage";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(505, 131);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 13);
+            this.label4.TabIndex = 22;
+            this.label4.Text = "Memory Usage";
+            this.label4.Click += new System.EventHandler(this.Label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(325, 131);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(73, 13);
+            this.label5.TabIndex = 23;
+            this.label5.Text = "Running Time";
+            this.label5.Click += new System.EventHandler(this.Label5_Click);
+            // 
+            // cpu
+            // 
+            this.cpu.Location = new System.Drawing.Point(508, 99);
+            this.cpu.Name = "cpu";
+            this.cpu.Size = new System.Drawing.Size(75, 20);
+            this.cpu.TabIndex = 24;
+            // 
+            // running
+            // 
+            this.running.Location = new System.Drawing.Point(328, 159);
+            this.running.Name = "running";
+            this.running.Size = new System.Drawing.Size(128, 20);
+            this.running.TabIndex = 25;
+            // 
+            // memory
+            // 
+            this.memory.Location = new System.Drawing.Point(508, 159);
+            this.memory.Name = "memory";
+            this.memory.Size = new System.Drawing.Size(75, 20);
+            this.memory.TabIndex = 26;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(329, 202);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(54, 13);
+            this.label7.TabIndex = 29;
+            this.label7.Text = "Comment ";
+            // 
+            // comment
+            // 
+            this.comment.Location = new System.Drawing.Point(328, 230);
+            this.comment.Name = "comment";
+            this.comment.Size = new System.Drawing.Size(212, 20);
+            this.comment.TabIndex = 30;
+            this.comment.TextChanged += new System.EventHandler(this.Comment_TextChanged);
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(719, 464);
+            this.Controls.Add(this.comment);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.memory);
+            this.Controls.Add(this.running);
+            this.Controls.Add(this.cpu);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listView);
+            this.Controls.Add(this.metroButton2);
+            this.Controls.Add(this.metroTextBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.471698F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Name = "Form";
+            this.Text = "All processes";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.ProcessForm_FormClosing);
+            this.Load += new System.EventHandler(this.ProcessForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private MetroFramework.Controls.MetroTextBox metroTextBox1;
+        private MetroFramework.Controls.MetroButton metroButton2;
+        private System.Windows.Forms.ListView listView;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox start;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox cpu;
+        private System.Windows.Forms.TextBox running;
+        private System.Windows.Forms.TextBox memory;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox comment;
+    }
+}
+
---
+++ Process-Note/ProcessForm.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Management;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Process_Note
+{
+    public partial class Form : MetroFramework.Forms.MetroForm
+    {
+        Dictionary<Process, string> processComment = new Dictionary<Process, string>();
+        IEnumerable<Process> processes;
+        public Form()
+        {
+            InitializeComponent();
+        }
+
+        void GetAllProcess()
+        {
+            processes = Process.GetProcesses().OrderBy(Process => Process.ProcessName);
+            listView.Items.Clear();
+            foreach (Process p in processes)
+            {
+                if (p != null && p.Id > 0)
+                {
+                    try
+                    {
+                        ListViewItem item = new ListViewItem();
+                        item.Text = p.Id.ToString();
+                        item.Tag = p;
+                        item.SubItems.Add(p.ProcessName);
+                        listView.Items.Add(item);
+                    }
+                    catch
+                    {
+                    }
+                }
+            }
+        }
+
+        public float GetCPUUsage(Process process)
+        {
+            var mos = new ManagementObjectSearcher("SELECT * FROM Win32_PerfRawData_PerfProc_Process");
+            var run1 = mos.Get().Cast<ManagementObject>().ToDictionary(mo => mo.Properties["Name"].Value, mo => mo);
+            Thread.Sleep(1500);
+            var run2 = mos.Get().Cast<ManagementObject>().ToDictionary(mo => mo.Properties["Name"].Value, mo => mo);
+            String processName = process.ProcessName;
+            if (!run2.ContainsKey(processName)) throw new Exception(string.Format("Process not found: {0}", processName));
+
+            string percentageProcessorTime = "PercentProcessorTime";
+            string total = "_Total";
+
+            ulong percentageDiff = (ulong)run2[processName][percentageProcessorTime] - (ulong)run1[processName][percentageProcessorTime];
+            ulong totalDiff = (ulong)run2[total][percentageProcessorTime] - (ulong)run1[total][percentageProcessorTime];
+
+            return ((float)percentageDiff / (float)totalDiff) * 100.0f;
+        }
+
+        private string GetRamUsage(Process process)
+        {
+            PerformanceCounter ramCounter = new PerformanceCounter("Process", "Private Bytes", process.ProcessName, true);
+            double ram = Math.Round(ramCounter.NextValue() / 1024 / 1024, 2);
+            return ram.ToString() + "MB";
+        }
+
+        private void ProcessForm_Load(object sender, EventArgs e)
+        {
+            GetAllProcess();
+        }
+
+        private string GetComment(Process process)
+        {
+            if (processComment.ContainsKey(process))
+            {
+                return processComment[process];
+            }
+            return "Add your comment here!";
+        }
+
+        private void ProcessForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("Do you want to close this application?\r\nAll your comments will be lost!",
+                "Exit", MessageBoxButtons.YesNo) == System.Windows.Forms.DialogResult.No)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void ListView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listView.SelectedItems.Count > 0)
+            {
+                Process proc = GetCurrentProcess();
+                textBox1.Text = proc.Threads.Count.ToString();
+                DateTime startTime = proc.StartTime;
+                TimeSpan runningTime = DateTime.Now - startTime;
+                running.Text = runningTime.ToString();
+                start.Text = startTime.ToString();
+                cpu.Text = GetCPUUsage(proc).ToString() + "%";
+                memory.Text = GetRamUsage(proc);
+                comment.Text = GetComment(proc);
+            }
+        }
+
+        private Process GetCurrentProcess()
+        {
+            if (listView.SelectedItems.Count > 0 && listView.SelectedItems[0] != null)
+            {
+                foreach (Process p in processes)
+                {
+                    if (p.Id.ToString() == listView.SelectedItems[0].Text)
+                    {
+                        return p;
+                    }
+                }
+            }
+            return null;
+        }
+        private void Comment_TextChanged(object sender, EventArgs e)
+        {
+            processComment[GetCurrentProcess()] = comment.Text;
+        }
+
+        private void MetroButton2_Click(object sender, EventArgs e)
+        {
+            ListView_SelectedIndexChanged(sender, e);
+        }
+        private void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MetroButton1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
